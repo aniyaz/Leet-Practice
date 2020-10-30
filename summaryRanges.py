18f7453d03f679d8bda238c9511c94d2e312c07b
Aman <amanniyaz.waplog@hotmail.com>
2020-10-30 23:55:24 +0000

    Solving summaryRanges

---
+++ summaryRanges.py
@@ -0,0 +1,9 @@
+#Link: https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3510/
+
+def summaryRanges(nums):
+    result = []
+    if len(nums) == 1:
+        return [str(nums[0])]
+    for i in range(len(nums)):
+        n1 = num[i]
+        if 
